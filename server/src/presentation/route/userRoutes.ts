c7c0e4d1607d2d5854a38b9a661a7eaefe6f5cd2
InnaMnatsakanyan <inna.mnacakanyan1@gmail.com>
2025-03-23 18:22:35 +0400

    final commit files

--- server/src/presentation/route/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userController from "../controller/userController";
-import authMiddleware from "../../middleware/authMiddleware";
-
-const router = express.Router()
-
-router.get('/', authMiddleware.authenticateToken, userController.currentUser)
-
-export default router
--- server/src/service/dbPool.ts
+++
@@ -1,9 +0,0 @@
-import {Pool} from "pg";
-
-export const pool = new Pool({
-    user: 'inmkn',
-    host: 'localhost',
-    database: 'rhymee',
-    password: 'ims4b7b9',
-    port: 5432,
-});
